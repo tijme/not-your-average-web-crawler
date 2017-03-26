432d82891475b1a1548069487062bec15ce88284
Tijme Gommers <tijme@finnwea.com>
2017-03-26 19:01:08 +0200

    Version update.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('requirements.txt') as file:
     requirements = file.read().splitlines()
 
 setup(
-    version="1.2.2",
+    version="1.2.3",
     name="nyawc",
     description="A web crawler that gathers requests instead of URLs.",
     long_description="A very useful web crawler for vulnerability scanning. Not Your Average Web Crawler (N.Y.A.W.C) is a Python application that enables you to crawl web applications for requests instead of URLs. It crawls every GET and POST request on the specified domain and keeps track of the request and response data. It's main purpose is to be used in web application vulnerability scanners",
