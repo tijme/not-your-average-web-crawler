d7cec41dcce62610014217a3216cc11ce37c86d8
Tijme Gommers <tijme@finnwea.com>
2017-06-28 18:34:49 +0200

    Added version 1.6.3.

--- releases.js
+++ releases.js
@@ -1,1 +1,1 @@
-var releases = {"1.4.7": true, "1.5.0": true, "1.5.1": true, "1.5.2": true, "1.6.0": true}
+var releases = {"1.4.7": true, "1.5.0": true, "1.5.1": true, "1.5.2": true, "1.6.0": true, "1.6.3": true}
