41f6b3ec16c68a12e0670297efb7c50655d7ef2f
Tijme Gommers <tijme@finnwea.com>
2017-07-10 20:21:14 +0200

    Fixed releases dropdown.

--- latest/_static/releases.js
+++ latest/_static/releases.js
@@ -1,1 +1,6 @@
-var releases = {"1.4.7": true, "1.5.0": true, "1.5.1": true, "1.5.2": true, "1.6.5\n": true}
+var releases = {
+    "1.4.7": true,
+    "1.5.0": true,
+    "1.5.1": true,
+    "1.5.2": true
+}
