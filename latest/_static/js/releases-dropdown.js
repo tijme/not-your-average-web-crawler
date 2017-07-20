6978e1362a14979eb15f792e09732e28b6ab93e4
Tijme Gommers <tijme@finnwea.com>
2017-07-20 16:05:04 +0200

    Added 1.7.1

--- latest/_static/js/releases-dropdown.js
+++ latest/_static/js/releases-dropdown.js
@@ -1,6 +1,3 @@
-var releases = {"1.4.7": true, "1.5.0": true, "1.5.1": true, "1.5.2": true, "1.6.3": true, "1.6.4": true, "1.6.5": true, "1.7.0": true}
-
-
 $(document).ready(function() {
     var latestVersion = Object.keys(releases)[Object.keys(releases).length - 1];
     var currentVersion = $('#releases').attr('data-selected');
