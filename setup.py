54646ad1000594b90c03e4fafa37de794168206e
tgommers <tijme.gommers@northwave.nl>
2019-02-01 23:04:48 +0100

    Removed support for Python 3.4 since the dependency 'requests' doesn't support it anymore.

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 2.7",
         "Topic :: Security"
